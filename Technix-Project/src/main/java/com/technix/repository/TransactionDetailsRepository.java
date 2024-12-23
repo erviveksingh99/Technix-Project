7be96e9baa652e9fc1076f0d6eb4783b53d50da3
Vivek Singh <erviveksingh6292@gmail.com>
2024-12-23 16:58:06 +0530

    TransactionDetailsRepo commit

--- Technix-Project/src/main/java/com/technix/repository/TransactionDetailsRepository.java
+++ Technix-Project/src/main/java/com/technix/repository/TransactionDetailsRepository.java
@@ -1,7 +1,11 @@
 package com.technix.repository;
 
 import com.technix.entity.TransactionDetails;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -9,5 +13,11 @@ import java.util.List;
 public interface TransactionDetailsRepository  extends JpaRepository<TransactionDetails, Integer> {
     List<TransactionDetails> findByTransactionNo(int transactionNo);
 
-    void deleteByTransactionId(int transactionId);
+
+   // void deleteByTransactionId(int transactionId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM TransactionDetails td WHERE td.transactionId = :transactionId")
+    void deleteByTransactionId(@Param("transactionId") int transactionId);
 }
